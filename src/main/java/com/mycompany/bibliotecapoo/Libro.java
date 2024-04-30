a85b4327c021a9ec165bcc1ac20d0cae9f765ea0
wilinton9 <159450242+wilinton9@users.noreply.github.com>
2024-04-29 22:23:15 -0500

    Update Libro.java

--- src/main/java/com/mycompany/bibliotecapoo/Libro.java
+++ src/main/java/com/mycompany/bibliotecapoo/Libro.java
@@ -1,38 +1,44 @@
 package com.mycompany.bibliotecapoo;
-
 public class Libro {
-    // TODO: Aquí va tu código
-    
+    // Atributos
     private String titulo;
     private String autor;
     private int annoPublicacion;
     private String genero;
     private boolean leido;
 
-    public Libro(String titulo, String autor, int annopublicacion, String genero, boolean leido) {
+    // Constructor
+    public Libro(String titulo, String autor, int annoPublicacion, String genero, boolean leido) {
         this.titulo = titulo;
         this.autor = autor;
-        this.annoPublicacion = annopublicacion;
+        this.annoPublicacion = annoPublicacion;
         this.genero = genero;
         this.leido = leido;
     }
     
-     public String mostrarInfor(){
-     return "Titulo:" + titulo + "Autor:" + autor + "Año de publicaion:" + annoPublicacion + "Genero:" + genero;
-            
-     }
-     public void marcarleido(){
-         leido = true;
-     }
-     public boolean esleido(){
-         return leido;
-     }
-     public boolean esAntiguo() {
-        if (annoPublicacion <1974){
-        return true;
-        }else {
-        return false;    
-        }
-      }
+    // Métodos
+    // Método para mostrar la información del libro
+    public String mostrarInfo() {
+        return "Titulo: " + titulo + ", Autor: " + autor + ", Año de publicación: " + annoPublicacion + ", Genero: " + genero;
+    }
+    
+    // Método para marcar el libro como leído
+    public void marcarLeido() {
+        leido = true;
+    }
+
+    // Método para verificar si el libro es antiguo
+    public boolean esAntiguo() {
+        int diferencia = 2024 - annoPublicacion;
+        return diferencia >= 50;
+    }
+
+    // Getters y Setters
+    public boolean getLeido() {
+        return leido;
+    }
+
+    public String getTitulo() {
+        return titulo;
     }
 }
